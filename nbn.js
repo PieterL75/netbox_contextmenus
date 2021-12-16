edb6faad560700c8807247cbe312afb5f5e55ac8
PieterL75 <74899468+PieterL75@users.noreply.github.com>
2021-12-16 12:43:28 +0100

    Small optimizations

--- nbn.js
+++ nbn.js
@@ -1,72 +1,72 @@
 const nbtn_views = {
     '/dcim/devices/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'Interfaces': ['interfaces/', 'mdi-dots-vertical'],
         'Console Ports': ['console-ports/', 'mdi-dots-vertical'],
         'Power Ports': ['power-ports/', 'mdi-dots-vertical'],
         'Inventory': ['inventory/', 'mdi-dots-vertical'],
         'Status': ['status/', 'mdi-dots-vertical'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/dcim/racks/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'Devices': ['/dcim/devices/?rack_id=$id$', 'mdi-dots-vertical'],
         'Power Feeds': ['/dcim/power-feeds/?rack_id=$id$', 'mdi-dots-vertical'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/ipam/ip-addresses/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/ipam/prefixes/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'Child Prefixes': ['prefixes/', 'mdi-chart-pie'],
         'Child IPranges': ['ip-ranges/', 'mdi-barcode'],
         'IP Addresses': ['ip-addresses/', 'mdi-dots-vertical'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/ipam/aggregates/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'Prefixes': ['/ipam/prefixes/?within_include=$obj$', 'mdi-chart-pie'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/ipam/vrfs/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'Prefixes': ['/ipam/prefixes/?vrf_id=$id$', 'mdi-dots-horizontal'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/ipam/vlans/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'Device Interfaces': ['interfaces/', 'mdi-chart-pie'],
         'VM Interfaces': ['vm-interfaces/', 'mdi-barcode'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/ipam/vlan-groups/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'VLANs': ['/ipam/vlans/?group_id=$id$', 'mdi-dots-horizontal'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/tenancy/tenants/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
         'VLANs': ['/ipam/vlans/?tenant_id=$id$', 'mdi-dots-horizontal'],
         'Prefixes': ['/ipam/prefixes/?tenant_id=$id$', 'mdi-dots-horizontal'],
         'Devices': ['/dcim/devices/?tenant_id=$id$', 'mdi-dots-horizontal'],
         'VMs': ['/virtualization/virtual-machines/?tenant_id=$id$', 'mdi-dots-horizontal'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
     '/tenancy/tenant-groups/': {
         'View': ['', 'mdi-share'],
-        'Edit': ['edit/?return_url=$return_url$', 'mdi-pencil'],
-        'Delete': ['delete/?return_url=$return_url$', 'mdi-delete'],
+        'Edit': ['edit/?return_url=$current_url$', 'mdi-pencil'],
+        'Delete': ['delete/?return_url=$current_url$', 'mdi-delete'],
     },
 };
 
@@ -102,7 +102,7 @@ function nbtnShowbox(e) {
                     if (uri.length>1) {
                         for (var vars of uri[1].split('&')) {
                             vars = vars.split(/=(.+)/)
-                            newurl.searchParams.set(vars[0], vars[1].replace('$id$', id).replace('$obj$', objtext).replace('$return_url$',window.location.href));
+                            newurl.searchParams.set(vars[0], vars[1].replace('$id$', id).replace('$obj$', objtext).replace('$current_url$',window.location.href));
                         }
                     }
                     nbtnmenu.innerHTML += '<li class="list-group-item list-group-item-action' + (items == 'Delete' ? ' trash' : '') + '"><a href="' + newurl + '"><i class="mdi ' + nbtn_views[view][items][1] + '"></i> ' + items + '</a></li>';
@@ -114,7 +114,6 @@ function nbtnShowbox(e) {
 
         var menuWidth = nbtnboxmenu.offsetWidth + 8;
         var menuHeight = nbtnboxmenu.offsetHeight + 8;
-
         var windowWidth = window.innerWidth;
         var windowHeight = window.innerHeight;
 
@@ -174,33 +173,41 @@ function nbtnShowbox(e) {
             var links = divs[i].getElementsByTagName('a');
             for (j = 0; j < links.length; j++) {
                 var link = links[j];
-                for (const view of Object.keys(nbtn_views)) {
-                    var uri = link.getAttribute("href")
-                    var parts = uri.split('/');
-                    var id = parts[3];
-                    var action = parts[4];
-                    if (link.getAttribute("href").startsWith(view) && !isNaN(0 + id) && !action && link.getElementsByTagName('i').length == 0 && link.innerText.trim().length > 0) {
-                        // url has to start with the nbtn_views key, and the id (3th part of the urlpath) has to be a number and the (4th partt of the urlpath) action has to be empty and there should be no <i> tag in the ahref and the ahref innerhtml is not empty.
+                var uri = link.getAttribute("href")
+                var parts = uri.split('/');
+                var id = parts[3];
+                var action = parts[4];
+                if (!isNaN(0 + id) && !action && link.getElementsByTagName('i').length == 0 && link.innerText.trim().length > 0) {
+                    // only add the menuicon to links that:
+                    //  - the id (3th part of the urlpath) has to be a number (excludes the generic urls)
+                    //  - the action (4th part of the urlpath) has to be empty (excludes the action buttons)
+                    //  - there should be no <i> tag in the ahref (those are the bullets for intended ip/prefix/vlan/...)
+                    //  - the ahref innerhtml is not empty.
+                    for (const view of Object.keys(nbtn_views)) {
+                        if (uri.startsWith(view)) {
+                            // only add the menuicon to links that:
+                            //  - start with the nbtn_views key
 
-                        var nbtnbox = link.appendChild(document.createElement("div"));
-                        nbtnbox.className = "nbtn-box";
-                        var nbtnspan = nbtnbox.appendChild(document.createElement("span"));
-                        nbtnspan.id = "nbtnbox";
-                        nbtnspan.className = "btn btn-sm nbtn-icon";
-                        nbtnspan.title = "Actions";
-                        var nbtnspani=nbtnspan.appendChild(document.createElement("i"));
-                        nbtnspani.className="mdi mdi-menu";
+                            var nbtnbox = link.appendChild(document.createElement("div"));
+                            nbtnbox.className = "nbtn-box";
+                            var nbtnspan = nbtnbox.appendChild(document.createElement("span"));
+                            nbtnspan.id = "nbtnbox";
+                            nbtnspan.className = "btn btn-sm nbtn-icon";
+                            nbtnspan.title = "Actions";
+                            var nbtnspani=nbtnspan.appendChild(document.createElement("i"));
+                            nbtnspani.className="mdi mdi-menu";
 
-                        link.style['white-space'] = 'nowrap';
+                            link.style['white-space'] = 'nowrap';
 
-                        nbtnbox.addEventListener('mouseover', function (e) {
-                            nbtnShowbox(e)
-                        }, false);
-                        nbtnbox.url = link.href;
+                            nbtnbox.addEventListener('mouseover', function (e) {
+                                nbtnShowbox(e)
+                            }, false);
+                            nbtnbox.url = link.href;
+                            break;
+                        }
                     }
                 }
             }
         }
     }
-
 })();
